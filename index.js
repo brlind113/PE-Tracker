8dbb0e65f05231b4552a245a43268d97e5de0d33
Brian <98599864+brlind113@users.noreply.github.com>
2022-04-03 19:13:59 -0500

    Add basic

--- index.js
+++ index.js
@@ -10,7 +10,6 @@ const pool = new Pool({
   }
 });
 
-
 express()
   .use(express.static(path.join(__dirname, 'public')))
   .use(express.json())
@@ -28,6 +27,32 @@ express()
         console.error(err);
         res.send("Error " + err);
       }
+    })
+      .get('/db-info', async(req, res) =>{
+        try {
+          const client = await pool.connect();
+          const tables = await client.query(
+          `SELECT c.relname AS table, a.attname AS column, t.typname AS type 
+          FROM pg_catalog.pg_class AS c 
+          LEFT JOIN pg_catalog.pg_attribute a 
+          ON c.oid = a.attrelid AND a.attnum > 0
+          LEFT JOIN pg_catalog.pg_type AS t
+          ON a.atttypid = t.oid 
+          WHERE c.relname IN ('users', 'observations', 'students', 
+          'schools', 'tasks')
+          ORDER BY c.relname, a.attnum`
+          );
+            const locals = {
+              'tables': (tables) ? tables.rows : null
+            };
+            
+            res.render('pages/db-info', locals);
+            client.release();
+        }
+        catch (err) {
+          console.error(err);
+          res.send("Error: " + err);
+        }
+      })
 
-  })
   .listen(PORT, () => console.log(`Listening on ${ PORT }`));
